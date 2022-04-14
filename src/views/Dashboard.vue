ba2a194ccc823556e147b38903d73c8d002494b3
Syifa Ainnur <arwahitem01@gmail.com>
2022-04-14 20:29:12 +0700

    penambahan chart

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -24,7 +24,8 @@
           :icon-classes="['ion', 'ion-ios-gear-outline']"
           text="CPU Traffic"
           number="90%"
-        ></info-box>
+        >
+        </info-box>
       </div>
       <!-- /.col -->
       <div class="col-md-3 col-sm-6 col-xs-12">
@@ -46,7 +47,8 @@
           :icon-classes="['ion', 'ion-ios-cart-outline']"
           text="Sales"
           number="760"
-        ></info-box>
+        >
+        </info-box>
       </div>
       <!-- /.col -->
       <div class="col-md-3 col-sm-6 col-xs-12">
@@ -66,19 +68,31 @@
         <div class="box-header with-border">
           <h3 class="box-title"></h3>
           <div class="box-body">
-            <div class="col-sm-6 col-xs-12">
+            <div class="col-sm-12">
               <p class="text-center">
                 <strong>Web Traffic Overview</strong>
               </p>
               <canvas id="trafficBar"></canvas>
             </div>
             <hr class="visible-xs-block" />
-            <div class="col-sm-6 col-xs-12">
-              <p class="text-center">
-                <strong>Language Overview</strong>
-              </p>
-              <canvas id="languagePie"></canvas>
+          </div>
+          <div class="d-flex justify-content-center">
+          <div class="box-body">
+            <div class="row">
+              <div class="col-sm-6">
+                <p class="text-center">
+                  <strong>Language Overview</strong>
+                </p>
+                <canvas id="languagePie"></canvas>
+              </div>
+              <div class="col-sm-6">
+                <p class="text-center">
+                  <strong> Overview</strong>
+                </p>
+                <canvas id="Overview"></canvas>
+              </div>
             </div>
+          </div>
           </div>
         </div>
         <div class="text-center">
@@ -120,7 +134,8 @@
           number="114,381"
           :progress="70"
           description="70% increase since yesterday"
-        ></process-info-box>
+        >
+        </process-info-box>
       </div>
       <!-- /.col -->
       <div class="col-md-3 col-sm-6 col-xs-12">
@@ -131,7 +146,8 @@
           number="163,921"
           :progress="40"
           description="40% increase compared to last year"
-        ></process-info-box>
+        >
+        </process-info-box>
       </div>
       <!-- /.col -->
     </div>
@@ -141,9 +157,9 @@
 </template>
 
 <script>
-// import Chart from 'chart.js'
+import Chart from "chart.js/auto";
 // import Alert from '../widgets/Alert'
-import InfoBox from '../views/widgets/infoBox.vue'
+import InfoBox from "../views/widgets/infoBox.vue";
 // import ProcessInfoBox from '../widgets/ProcessInfoBox'
 
 export default {
@@ -153,123 +169,152 @@ export default {
     InfoBox,
     // ProcessInfoBox
   },
-  // data() {
-  //   return {
-  //     generateRandomNumbers(numbers, max, min) {
-  //       var a = [];
-  //       for (var i = 0; i < numbers; i++) {
-  //         a.push(Math.floor(Math.random() * (max - min + 1)) + max);
-  //       }
-  //       return a;
-  //     },
-  //   };
-  // },
-  // computed: {
-  //   coPilotNumbers() {
-  //     return this.generateRandomNumbers(12, 1000000, 10000);
-  //   },
-  //   personalNumbers() {
-  //     return this.generateRandomNumbers(12, 1000000, 10000);
-  //   },
-  //   isMobile() {
-  //     return window.innerWidth <= 800 && window.innerHeight <= 600;
-  //   },
-  // },
-  // mounted() {
-  //   this.$nextTick(() => {
-  //     var ctx = document.getElementById("trafficBar").getContext("2d");
-  //     var config = {
-  //       type: "line",
-  //       data: {
-  //         labels: [
-  //           "January",
-  //           "February",
-  //           "March",
-  //           "April",
-  //           "May",
-  //           "June",
-  //           "July",
-  //           "August",
-  //           "September",
-  //           "October",
-  //           "November",
-  //           "December",
-  //         ],
-  //         datasets: [
-  //           {
-  //             label: "CoPilot",
-  //             fill: false,
-  //             borderColor: "#284184",
-  //             pointBackgroundColor: "#284184",
-  //             backgroundColor: "rgba(0, 0, 0, 0)",
-  //             data: this.coPilotNumbers,
-  //           },
-  //           {
-  //             label: "Personal Site",
-  //             borderColor: "#4BC0C0",
-  //             pointBackgroundColor: "#4BC0C0",
-  //             backgroundColor: "rgba(0, 0, 0, 0)",
-  //             data: this.personalNumbers,
-  //           },
-  //         ],
-  //       },
-  //       options: {
-  //         responsive: true,
-  //         maintainAspectRatio: !this.isMobile,
-  //         legend: {
-  //           position: "bottom",
-  //           display: true,
-  //         },
-  //         tooltips: {
-  //           mode: "label",
-  //           xPadding: 10,
-  //           yPadding: 10,
-  //           bodySpacing: 10,
-  //         },
-  //       },
-  //     };
+  data() {
+    return {
+      generateRandomNumbers(numbers, max, min) {
+        var a = [];
+        for (var i = 0; i < numbers; i++) {
+          a.push(Math.floor(Math.random() * (max - min + 1)) + max);
+        }
+        return a;
+      },
+    };
+  },
+  computed: {
+    coPilotNumbers() {
+      return this.generateRandomNumbers(12, 1000000, 10000);
+    },
+    personalNumbers() {
+      return this.generateRandomNumbers(12, 1000000, 10000);
+    },
+    isMobile() {
+      return window.innerWidth <= 800 && window.innerHeight <= 600;
+    },
+  },
+  mounted() {
+    this.$nextTick(() => {
+      var ctx = document.getElementById("trafficBar").getContext("2d");
+      var config = {
+        type: "line",
+        data: {
+          labels: [
+            "January",
+            "February",
+            "March",
+            "April",
+            "May",
+            "June",
+            "July",
+            "August",
+            "September",
+            "October",
+            "November",
+            "December",
+          ],
+          datasets: [
+            {
+              label: "CoPilot",
+              fill: false,
+              borderColor: "#284184",
+              pointBackgroundColor: "#284184",
+              backgroundColor: "rgba(0, 0, 0, 0)",
+              data: this.coPilotNumbers,
+            },
+            {
+              label: "Personal Site",
+              borderColor: "#4BC0C0",
+              pointBackgroundColor: "#4BC0C0",
+              backgroundColor: "rgba(0, 0, 0, 0)",
+              data: this.personalNumbers,
+            },
+          ],
+        },
+        options: {
+          responsive: true,
+          maintainAspectRatio: !this.isMobile,
+          legend: {
+            position: "bottom",
+            display: true,
+          },
+          tooltips: {
+            mode: "label",
+            xPadding: 10,
+            yPadding: 10,
+            bodySpacing: 10,
+          },
+        },
+      };
 
-  //     new Chart(ctx, config); // eslint-disable-line no-new
+      new Chart(ctx, config); // eslint-disable-line no-new
 
-  //     var pieChartCanvas = document
-  //       .getElementById("languagePie")
-  //       .getContext("2d");
-  //     var pieConfig = {
-  //       type: "pie",
-  //       data: {
-  //         labels: ["HTML", "JavaScript", "CSS"],
-  //         datasets: [
-  //           {
-  //             data: [56.6, 37.7, 4.1],
-  //             backgroundColor: ["#00a65a", "#f39c12", "#00c0ef"],
-  //             hoverBackgroundColor: ["#00a65a", "#f39c12", "#00c0ef"],
-  //           },
-  //         ],
-  //       },
-  //       options: {
-  //         responsive: true,
-  //         maintainAspectRatio: !this.isMobile,
-  //         legend: {
-  //           position: "bottom",
-  //           display: true,
-  //         },
-  //       },
-  //     };
+      var pieChartCanvas = document
+        .getElementById("languagePie")
+        .getContext("2d");
+      var pieConfig = {
+        type: "pie",
+        data: {
+          labels: ["HTML", "JavaScript", "CSS"],
+          datasets: [
+            {
+              data: [56.6, 37.7, 4.1],
+              backgroundColor: ["#00a65a", "#f39c12", "#00c0ef"],
+              hoverBackgroundColor: ["#00a65a", "#f39c12", "#00c0ef"],
+            },
+          ],
+        },
+        options: {
+          responsive: true,
+          maintainAspectRatio: !this.isMobile,
+          legend: {
+            position: "bottom",
+            display: true,
+          },
+        },
+      };
 
-  //     new Chart(pieChartCanvas, pieConfig); // eslint-disable-line no-new
-  //   });
-  // },
+      new Chart(pieChartCanvas, pieConfig);
+
+      var pieChartCanvas = document.getElementById("Overview").getContext("2d");
+      var pieConfig = {
+        type: "pie",
+        data: {
+          labels: ["HTML", "JavaScript", "CSS"],
+          datasets: [
+            {
+              data: [56.6, 37.7, 4.1],
+              backgroundColor: ["#00a65a", "#f39c12", "#00c0ef"],
+              hoverBackgroundColor: ["#00a65a", "#f39c12", "#00c0ef"],
+            },
+          ],
+        },
+        options: {
+          responsive: true,
+          maintainAspectRatio: !this.isMobile,
+          legend: {
+            position: "bottom",
+            display: true,
+          },
+        },
+      };
+
+      new Chart(pieChartCanvas, pieConfig);
+
+      // eslint-disable-line no-new
+    });
+  },
 };
 </script>
 <style>
 .info-box {
   cursor: pointer;
 }
+
 .info-box-content {
   text-align: center;
   vertical-align: middle;
   display: inherit;
 }
+
 .fullCanvas {
   width: 100%;
 }
